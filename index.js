d3d8d5904c27f9f225dc170fc825c47c5298b4f5
Neha Chopade <nehachopade22@gmail.com>
2018-08-19 19:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,3 @@ function ternaryCheckCity(city){
   
 }
 
-/*describe('ternaryCheckCity()', function () {
-    it('returns "Ok, sounds good." when the city is NYC', function () {
-      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-    });
-
-    it('should return "No go." if the destination city is not NYC', function () {
-      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
-    });
-  });*/
