069c6a6c4a763a1e690a46b391a39bcbb41f0ce8
Neha Chopade <nehachopade22@gmail.com>
2018-08-19 19:43:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber){
+   
+   if (someNumber<=400)
+    {
+      return 'This one is on me!'
+    } else if (someNumber>2000 && someNumber<=2500){
+      return 'I will gladly take your thirty bucks.'
+    } else if (someNumber>=2501){
+      return 'No can do.'
+    }
+}
+function ternaryCheckCity(city){
+  if (city=='NYC')
+  { return 'Ok, sounds good.' }
+  return 'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous' : return "Thank you so much.";
+    case  'not as generous' : return "Thank you.";
+    default :  return 'Bye.';
+  }
+}
