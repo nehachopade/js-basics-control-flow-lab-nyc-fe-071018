5078639810b7f4acc375df74107e7cbe6c5ebac9
Neha Chopade <nehachopade22@gmail.com>
2018-08-19 19:36:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ function scuberGreetingForFeet(someNumber){
     }
 }
 function ternaryCheckCity(city){
-  
+  if(city=='NYC'){return 'Ok, sounds good.' }
+  return No go.
   
 }
+
+/*describe('ternaryCheckCity()', function () {
+    it('returns "Ok, sounds good." when the city is NYC', function () {
+      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+    });
+
+    it('should return "No go." if the destination city is not NYC', function () {
+      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+    });
+  });*/
