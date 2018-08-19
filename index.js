490678631c8f1168202838bf0b3d85cb6f077779
Neha Chopade <nehachopade22@gmail.com>
2018-08-19 19:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber){
+   let result;
+   if (result==someNumber)
+    {
+      return "This one is on me!"
+    } else if ()
+    
+   
+}
+scuberGreetingForFeet(400);
+
+function ternaryCheckCity(){
+  
+}
+
+switchOnCharmFromTip(){
+  switch(){
+    case:
+    case:
+  }
+  
+}
