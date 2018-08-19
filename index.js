5ff997669d430ed30a53b8260f4e94c2b146bbdf
Neha Chopade <nehachopade22@gmail.com>
2018-08-19 19:27:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function scuberGreetingForFeet(someNumber){
    
    if (someNumber<=400)
     {
-      return "This one is on me!"
+      return 'This one is on me!'
     } else if (someNumber>2000){
-      
-    }
+      return 'I will gladly take your thirty bucks.'
+    } else if (someNumner)
 }
